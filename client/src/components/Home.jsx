f2d73a113dc8639b428b2f8eb81f526606503f1c
Scott <scottepick@gmail.com>
2020-06-16 17:56:08 -0400

    changes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Layout from './shared/Layout'
 import AllPosts from './AllPosts'
-
+////home
 const Home = () => {
-  return (
-    <Layout>
-      <div className="all-posts">
-        <AllPosts />
-      </div>
-    </Layout>
-  )
+ return (
+  <Layout>
+   <div className="all-posts">
+    <AllPosts />
+   </div>
+  </Layout>
+ )
 }
 export default Home;
