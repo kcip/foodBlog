99a628604f26a52202cf7ef924dd4bf2ff420361
Scott <scottepick@gmail.com>
2020-06-17 17:29:37 -0400

    fixed redirect bug

--- client/src/components/PostEdit.jsx
+++ client/src/components/PostEdit.jsx
@@ -4,96 +4,96 @@ import { getPost, updatePost } from '../services/posts'
 import { Redirect } from 'react-router-dom'
 
 class PostEdit extends Component {
-  constructor(props) {
-    super()
-    this.state = {
-      post: {
-        title: '',
-        user: '',
-        imgURL: '',
-        content: '',
-        _id: ''
-      },
-      updated: false
-    }
+ constructor(props) {
+  super()
+  this.state = {
+   post: {
+    title: '',
+    user: '',
+    imgURL: '',
+    content: '',
+    _id: ''
+   },
+   updated: false
+  }
+ }
+
+
+ async componentDidMount() {
+  let { id } = this.props.match.params
+  const post = await getPost(id)
+  this.setState({ post })
+ }
+
+
+ handleChange = (event) => {
+  const { name, value } = event.target
+  this.setState({
+   post: {
+    ...this.state.post,
+    [name]: value
+   }
+  })
+ }
+
+ handleSubmit = async (event) => {
+  event.preventDefault()
+  let { id } = this.props.match.params
+  const updated = await updatePost(id, this.state.post)
+  this.setState({ updated })
+ }
+ render() {
+
+  const { post, updated } = this.state
+
+  if (updated) {
+   return <Redirect to={`/` + post._id} />
   }
 
 
-  async componentDidMount() {
-    let { id } = this.props.match.params
-    const post = await getPost(id)
-    this.setState({ post })
-  }
-
-
-  handleChange = (event) => {
-    const { name, value } = event.target
-    this.setState({
-      post: {
-        ...this.state.post,
-        [name]: value
-      }
-    })
-  }
-
-  handleSubmit = async (event) => {
-    event.preventDefault()
-    let { id } = this.props.match.params
-    const updated = await updatePost(id, this.state.post)
-    this.setState({ updated })
-  }
-  render() {
-
-    const { post, updated } = this.state
-
-    if (updated) {
-      return <Redirect to={`${this.props.match.params.id}`} />
-    }
-
-
-    return (
-      <Layout>
-        <div className="product-edit">
-          <form className="postCreate" onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              className="input-name"
-              placeholder='Title'
-              value={post.title}
-              name='title'
-              required
-              autoFocus
-              onChange={this.handleChange} />
-            <input
-              type="text"
-              className="input-imgURL"
-              placeholder='Image URL'
-              value={post.imgURL}
-              name='imgURL'
-              required
-              onChange={this.handleChange} />
-            <textarea
-              type="text"
-              className="textarea-content"
-              rows={20}
-              placeholder='Content'
-              value={post.content}
-              name='content'
-              required
-              onChange={this.handleChange} />
-            <input
-              type="text"
-              className="input-user"
-              placeholder='Author Name'
-              value={post.user}
-              name='user'
-              required
-              onChange={this.handleChange} />
-            <button type='submit' className="submit-button">Submit</button>
-          </form>
-        </div>
-      </Layout>
-    )
-  }
+  return (
+   <Layout>
+    <div className="product-edit">
+     <form className="postCreate" onSubmit={this.handleSubmit}>
+      <input
+       type="text"
+       className="input-name"
+       placeholder='Title'
+       value={post.title}
+       name='title'
+       required
+       autoFocus
+       onChange={this.handleChange} />
+      <input
+       type="text"
+       className="input-imgURL"
+       placeholder='Image URL'
+       value={post.imgURL}
+       name='imgURL'
+       required
+       onChange={this.handleChange} />
+      <textarea
+       type="text"
+       className="textarea-content"
+       rows={20}
+       placeholder='Content'
+       value={post.content}
+       name='content'
+       required
+       onChange={this.handleChange} />
+      <input
+       type="text"
+       className="input-user"
+       placeholder='Author Name'
+       value={post.user}
+       name='user'
+       required
+       onChange={this.handleChange} />
+      <button type='submit' className="submit-button">Submit</button>
+     </form>
+    </div>
+   </Layout>
+  )
+ }
 }
 export default PostEdit;
